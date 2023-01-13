dd981bb28afb407a379b5a21ea9fad9fd9736948
KaraJH <karas.jones@yahoo.com>
2023-01-13 16:01:42 -0700

    test: Braille characters and braille dictionary methods

--- spec/dictionary_spec.rb
+++ spec/dictionary_spec.rb
@@ -8,4 +8,11 @@ RSpec.describe Dictionary do
       expect(dictionary).to be_a(Dictionary)
     end
   end
+
+  describe '#braille_dictionary and braille_characters' do 
+    it 'is initialized with a dictionary of braille characters' do 
+      expect(braille_characters.keys).to include("a", "b", "c", "l", "m", "n")
+      expect(braille_dictionary.class).to be_a(Hash)
+    end
+  end
 end
