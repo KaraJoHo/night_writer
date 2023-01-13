e31e4798fad17051ad14bcbe6db1209d98bea9ce
KaraJH <karas.jones@yahoo.com>
2023-01-13 16:07:31 -0700

    test: English characters and to english methods

--- spec/dictionary_spec.rb
+++ spec/dictionary_spec.rb
@@ -12,7 +12,16 @@ RSpec.describe Dictionary do
   describe '#braille_dictionary and braille_characters' do 
     it 'is initialized with a dictionary of braille characters' do 
       expect(dictionary.braille_characters.keys).to include("a", "b", "c", "l", "m", "n")
+      expect(dictionary.braille_characters.values).to include(["0.", "..", ".."], ["00", "00", ".."])
       expect(dictionary.braille_dictionary).to be_a(Hash)
+    end
+  end
+
+  describe '#to_english and english_characters' do 
+    it 'is initialized with a dictionary of braille to english characters' do 
+      expect(dictionary.english_characters.keys).to include(["0.", "..", ".."], ["00", "00", ".."])
+      expect(dictionary.english_characters.keys).to include("a", "b", "c", "l", "m", "n")
+      expect(dictionary.to_english_dictionary).to be_a(Hash)
     end
   end
 end
