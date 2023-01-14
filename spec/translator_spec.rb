03295f6c500bf2167dd6a54f62f57b8c6d025029
KaraJH <karas.jones@yahoo.com>
2023-01-14 15:50:36 -0700

    fix: Spelling error

--- spec/translator_spec.rb
+++ spec/translator_spec.rb
@@ -2,7 +2,7 @@ require_relative 'spec_helper'
 
 RSpec.describe Translator do 
   let(:translator) {Translator.new}
-  let(:dicationary) {Dictionary.new}
+  let(:dictionary) {Dictionary.new}
 
   describe '#translator' do 
     it 'exists and has attributes' do 
