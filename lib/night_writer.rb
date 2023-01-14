46e169aa813ac7e18f6d71904136b032dc4233fb
KaraJH <karas.jones@yahoo.com>
2023-01-14 16:19:29 -0700

    feat: Translate to braille with passing test

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -4,4 +4,9 @@ class NightWriter
   def initialize
     @translator = Translator.new
   end 
+
+  def translate_to_braille(message)
+    tr_message = @translator.eng_to_braille(message)
+    @translator.format_the_translation(tr_message)
+  end
 end
