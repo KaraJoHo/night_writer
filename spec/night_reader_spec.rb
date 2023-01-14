16b0f037e090b9d5176a9eb21083d5fdac98cfa6
KaraJH <karas.jones@yahoo.com>
2023-01-14 16:28:53 -0700

    test: Translate to english method

--- spec/night_reader_spec.rb
+++ spec/night_reader_spec.rb
@@ -10,4 +10,14 @@ RSpec.describe NightReader do
       expect(night_reader.translator).to be_a(Translator)
     end
   end
+
+  describe '#translate_to_braille' do 
+    it 'takes a string and translates it to braille' do 
+    
+      braille = "0.0.00\n00.0.0\n....00"
+      expected = 'hey'
+      
+      expect(night_writer.translate_to_braille(message)).to eq(expected)
+    end
+  end
 end
