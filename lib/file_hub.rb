ba93430507bdfdd707f72654947569e3063c864e
KaraJH <karas.jones@yahoo.com>
2023-01-15 13:09:46 -0700

    feat: Read message method

--- lib/file_hub.rb
+++ lib/file_hub.rb
@@ -9,4 +9,10 @@ class FileHub
     @translated_to_english = ""
   end
 
+  def read_message 
+    message = File.read(ARGV[0])
+    puts "created #{ARGV[1]} containing #{message.chars.count} characters."
+    @translated_to_braille = @night_writer.translate_to_braille(message)
+  end
+
 end
